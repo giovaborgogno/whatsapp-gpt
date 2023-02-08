9ca6d0ae79e3bb9aef3db1314330621ba83c3f58
root <root@vps.server.local>
2023-02-08 16:06:47 +0000

    correcciones

--- chatgpt.py
+++ chatgpt.py
@@ -8,22 +8,25 @@ class ChatGPT:
 
     start_prompt = '\nHuman: '
     restart_prompt = '\nGPT3: '
-    
+
     def send_prompt(self, user, prompt, context=''):
         prompt = context + self.start_prompt + prompt + self.restart_prompt
-        
+
         if len(prompt)>4000:
             prompt = prompt[-4000:]
-        
+
         response = openai.Completion.create(
             model="text-davinci-003", 
             prompt=prompt, 
             temperature=0, 
-            max_tokens=2000,
+            max_tokens=2500,
             user=user)
-        
-        context = prompt + response.choices[0].text
-        Response = {'response' : response.choices[0].text , 'context' : context}
 
+        response_text = response.choices[0].text
+        # limit caracters in whatsapp message is 1600
+        if len(response_text)>1599:
+            response_text = response_text[:1599]
+
+        context = prompt + response_text
+        Response = {'response' : response_text , 'context' : context}
         return Response
-        # return response.choices[0].text
